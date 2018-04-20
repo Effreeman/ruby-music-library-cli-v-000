6e882ecdea8b968113a7164ddc86777945b0b8e0
Emma Freeman <freemoe04@gmail.com>
2018-04-20 14:00:13 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,14 @@
 module Concerns::Findable
 
   def find_by_name(name)
-    self.all.find do |object|
-      object.name == name
-    end
-  end
+  all.detect {|x| x.name == name}
+end
 
-end 
+def find_or_create_by_name(name)
+  if find_by_name(name)
+    find_by_name(name)
+  else
+    create(name)
+  end
+end
+end
--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -8,10 +8,10 @@ class MusicImporter
   def files
       Dir.entries(path).select{|f|f.include?(".mp3")}
     end
-    
-    def import 
+
+    def import
       files.each {|filename| Song.create_from_filename(filename)}
     end
-    
-    
+
+
   end
