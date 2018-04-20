a97de3485069a9087f0bd72b6dd3b208d670f948
Emma Freeman <freemoe04@gmail.com>
2018-04-20 15:37:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,38 +1,38 @@
-class Artist 
+class Artist
   @@all = []
-  attr_accessor :name, :songs 
+  attr_accessor :name, :songs
   extend Concerns::Findable
   def initialize(name)
-    @name = name 
+    @name = name
     @songs = []
   end
-  
-  def self.all 
-    @@all 
+
+  def self.all
+    @@all
   end
-  
+
   def self.destroy_all
     @@all.clear
   end
-  
-  def save 
+
+  def save
     @@all << self
   end
-  
+
   def self.create(name)
     artist = self.new(name)
-    artist.save 
+    artist.save
     artist
   end
-  
+
   def add_song(song)
     if !@songs.include?(song)
-      @songs << song 
-      song.artist = self if song.artist.nil? 
+      @songs << song
+      song.artist = self if song.artist.nil?
     end
   end
-  
-  def genres 
-    @songs.collect {|song| song.genre}.uniq 
+
+  def genres
+    @songs.collect {|song| song.genre}.uniq
   end
 end
