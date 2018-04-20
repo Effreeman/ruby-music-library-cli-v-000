bf716b63f8f95ba8a557d752544d4fdaf07c0520
Emma Freeman <freemoe04@gmail.com>
2018-04-20 12:44:45 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,34 @@
+class Genre
+  attr_accessor :songs, :name
+  @@all = []
+
+    def initialize(name)
+      @name = name
+      @songs = []
+      @artists = artists
+      end
+
+    def self.all
+      @@all
+    end
+
+    def self.destroy_all
+      @@all = []
+    end
+
+    def save
+      @@all << self
+    end
+
+    def self.create(name)
+      new_genre = self.new(name)
+      new_genre.save
+      new_genre
+    end
+
+    def artists
+      self.songs.collect do |song|
+        song.artist
+      end.uniq
+    end
+end
