172a5d4899cc1964ad1a22b32ffb56eb9cf06e23
Emma Freeman <freemoe04@gmail.com>
2018-04-20 13:57:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,65 +1,63 @@
-class Song
-  attr_accessor :name, :genre
-  attr_reader :artist
-  extend Concerns::Findable
-    @@all = []
-
-    def initialize(name, artist = nil, genre = nil)
-      @name = name
-        if artist
-          self.artist = artist
-        end
-
-        if genre
-          self.genre = genre
-        end
-      end
-
-    def self.all
-      @@all
-    end
-
-    def self.destrpy_all
-      @@all.clear 
-    end
-
-    def save
-      @@all << self
-    end
-
-    def self.create(name)
-      new_song = self.new(name)
-      new_song.save
-      new_song
-    end
-
-    def artist=(artist)
-      @artist = artist
-      artist.add_song(self)
-    end
-
-    def genre=(genre)
-      @genre = genre
-      if !genre.songs.include?(self)
-        genre.songs << self
-      end
-
-    end
-
-    def self.new_from_filename(filename)
-    song = Song.new(filename.split(' - ')[1])
-    artist_name = filename.split(' - ')[0]
-    genre_name = filename.split(' - ')[2].chomp(".mp3")
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-    song.artist = artist
-    song.genre = genre
+class Song 
+  @@all = []
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist = nil, genre = nil)
+    @name = name 
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save 
+    @@all << self
+  end
+  
+  def self.create(name)
+    song = self.new(name)
+    song.save 
     song
   end
-
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def genre=(genre)
+    @genre = genre 
+    genre.add_song(self) 
+  end
+  
+  def self.find_by_name(name)
+    @@all.detect {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create(name)
+    end
+  end
+  
+  def self.new_from_filename(filename)
+    info = File.basename(filename, ".mp3").split(' - ')
+    Song.new(info[1], Artist.find_or_create_by_name(info[0]), Genre.find_or_create_by_name(info[2]))
+  end
+  
   def self.create_from_filename(filename)
     song = self.new_from_filename(filename)
-    song.save
+    song.save 
+    song
   end
+    
+  
 
 end
